20115873afc71e171db43b8d5a0580547fc73dfa
Travis Tabbal <ttabbal@solutionstream.com>
2013-06-06 22:52:10 -0600

    Add syncClassName property to debug properties. Default to existing implementation. Log the dynamic load.

--- app/src/com/newsrob/SyncInterfaceFactory.java
+++ app/src/com/newsrob/SyncInterfaceFactory.java
@@ -1,9 +1,29 @@
 package com.newsrob;
 
+import java.lang.reflect.Constructor;
+
 import android.content.Context;
+import android.util.Log;
 
 public class SyncInterfaceFactory {
+    private static final String TAG = SyncInterfaceFactory.class.getName();
+
     public static SyncInterface getSyncInterface(Context context) {
-        return new EntriesRetriever(context);
+        String className = NewsRob.getDebugProperties(context).getProperty("syncClassName",
+                "com.newsrob.EntriesRetriever");
+
+        Log.d(TAG, "Attempting to load sync class: " + className);
+
+        try {
+            Class<?> syncClass = Class.forName(className);
+            Log.d(TAG, "Class loaded: " + syncClass.toString());
+            Constructor<Context> constructor = (Constructor<Context>) syncClass.getConstructor(Context.class);
+            Log.d(TAG, "Constructor loaded: " + constructor.toString());
+            SyncInterface obj = (SyncInterface) constructor.newInstance(context);
+            Log.d(TAG, "SyncInterface loaded: " + obj.toString());
+            return obj;
+        } catch (Exception e) {
+            return new EntriesRetriever(context);
+        }
     }
 }
